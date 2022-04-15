94ede1562f4d5cf453bf0a6d2e6c8ef9bfee337b
baltikum <78216242+baltikum@users.noreply.github.com>
2022-04-15 10:04:51 +0200

    Add files via upload

---
+++ backend/Vcard.py
@@ -0,0 +1,7 @@
+
+
+class vcard:
+  def __init__(self, name, lastname, tel):
+      self.n = name
+      self.fn = lastname
+      self.tel = tel
---
+++ backend/server.py
@@ -0,0 +1,100 @@
+from flask import Flask,request,render_template,redirect
+import vobject
+import re
+import Vcard
+
+
+
+cardList = []
+brokenCards = []
+parsedCards = []
+vcardlist = []
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route("/load", methods = ['POST'] )
+def load_contacts():
+    global cardList,brokenCards,parsedCards
+
+    fil = request.get_data(True,True,False)
+
+    data = str(fil)
+    data = data.splitlines()
+    count = 0
+    if data:
+        start = False
+        card = False
+        for line in data:
+
+            if not card:
+                newCard = ''
+
+            if line.startswith('BEGIN:VCARD'):
+                start = True
+                card = True
+            elif line.startswith('END:VCARD'):
+                card = False
+
+            if start:
+                newCard = newCard + line + '\n'
+            
+            if not card :
+                if start:
+                    cardList.append(newCard)
+
+       
+        for entry in cardList:
+            try:
+                temp = vobject.readOne(entry)
+                parsedCards.append(temp)
+            except:
+                brokenCards.append(entry)
+
+        cardList = []
+        for i in range(len(parsedCards)):
+
+            try:
+                n = parsedCards[i].n.value
+            except AttributeError as e:
+                n = ''
+            try:
+                fn = parsedCards[i].fn.value
+            except AttributeError as e:
+                fn = ''
+            try:
+                tel = parsedCards[i].tel.value
+            except AttributeError as e:
+                tel = ''
+
+            temp = Vcard.vcard(n,fn,tel)
+            vcardlist.append(temp)
+            print(f'name:{n},  lastname:{fn},  tel:{tel}')
+
+
+    return redirect('/')
+
+
+@app.route("/clear", methods = ['GET'] )
+def clear_contacts():
+    global cardList,parsedCards,brokenCards
+    cardList = []
+    parsedCards = []
+    brokenCards = []
+    vcardlist = []
+    return {'contacts': ' empty'}
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
