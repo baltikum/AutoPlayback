4b05697e39b42896e46a3b2caff905c67be40771
baltikum <mwdavidsson@outlook.com>
2022-06-11 12:47:32 +0200

    hej

--- backend/pack/db_models.py
+++ backend/pack/db_models.py
@@ -1,21 +1,21 @@
-#from flask_sqlalchemy import SQLAlchemy
-#from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from datetime import datetime
-from pack import db
-#import os
+#from pack import db
+import os
 
-#db_username = os.environ['DB_USER']
-#db_password = os.environ['DB_PASSW']
-#db_address = os.environ['DB_ADDRESS']
-#db_name = os.environ['DB_NAME']
+db_username = os.environ['DB_USER']
+db_password = os.environ['DB_PASSW']
+db_address = os.environ['DB_ADDRESS']
+db_name = os.environ['DB_NAME']
 #autoplayback_db
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
 #Databas
-#app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_username}:{db_password}#@localhost/{db_name}'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_username}:{db_password}#@localhost/{db_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 #database table model for users
